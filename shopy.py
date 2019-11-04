a0ad0cbb2d32927b0c40828f1bd263258d674243
XinYang-YXY <yu.xin.yang.yxy@gmail.com>
2019-11-05 00:19:42 +0800

    Working on the forms

--- shopy.py
+++ shopy.py
@@ -21,7 +21,14 @@ def register():
     return render_template('register.html', form=form)
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form)
 
 
 if __name__ == "__main__":
