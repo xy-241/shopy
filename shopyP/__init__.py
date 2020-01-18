619555888456389a08ac6c13c0739e31654069ba
XinYang-YXY <yu.xin.yang.yxy@gmail.com>
2020-01-18 19:06:37 +0800

    Finished footer, polished a bit for technical review 1

--- shopyP/__init__.py
+++ shopyP/__init__.py
@@ -34,32 +34,57 @@ mail = Mail(app)
 from shopyP import routes
 # from flask_login import UserMixin # used for login-auth, To satisfy the login_manager requirements
 # from datetime import datetime
-# class User(db.Model, UserMixin):
+# class Person():
 #     id = db.Column(db.Integer, primary_key=True)
 #     username = db.Column(db.String(20), unique=True, nullable=False)
 #     email = db.Column(db.String(120), unique=True, nullable=False)
 #     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
 #     password = db.Column(db.String(60), nullable=False)
+#
+#
+#
+#     def __repr__(self):
+#         return f"Person('{self.username}', '{self.email}', '{self.image_file}')"
+# # Class format for user
+# class User(db.Model, Person, UserMixin):
 #     cart = db.relationship('CartItem', backref='owner')
+#
+#
+#
+#     def get_reset_token(self, expires_sec=1800):
+#         s = Serializer(app.config['SECRET_KEY'], expires_sec)
+#         return s.dumps({'user_id': self.id}).decode('utf-8')
+#
+#     @staticmethod # Telling the python not to take self as a paramenter
+#     def verify_reset_token(token):
+#         s = Serializer(app.config['SECRET_KEY'])
+#         # Incase the token is expired
+#         try:
+#             user_id = s.loads(token)['user_id']
+#         except:
+#             return None
+#         return User.query.get(user_id)
 #
 #     def __repr__(self):
 #         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 # # Class format for admin
-# class Admin(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(20), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-#     password = db.Column(db.String(60), nullable=False)
+# class Admin(db.Model, Person, UserMixin):
 #     def __repr__(self):
-#         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-# # Cart item
-# class CartItem(db.Model):
+#         return f"Admin('{self.username}', '{self.email}', '{self.image_file}')"
+#
+# # Parent Class for the CartItem, HackingProduct
+# class GeneralGoods():
 #     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(100), nullable=False)
+#
 #     price = db.Column(db.Float)
 #     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
 #     date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+#
+#     def __repr__(self):
+#         return f"GeneralGoods('{self.title}', 'S${self.price}', 'Date added:{self.date_added}')"
+# # Cart item
+# class CartItem(db.Model, GeneralGoods):
+#     title = db.Column(db.String(100), nullable=False)
 #     itemNum = db.Column(db.Integer, nullable=False)
 #     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 #
@@ -67,14 +92,10 @@ from shopyP import routes
 #         return f"CartItem('{self.title}', 'S${self.price}', 'Date added:{self.date_added}')"
 #
 # # Hacking products to sell
-# class HackingProduct(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
+# class HackingProduct(db.Model, GeneralGoods):
 #     title = db.Column(db.String(100), nullable=False, unique=True)
-#     price = db.Column(db.Float, nullable=False)
-#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-#     date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 #     description = db.Column(db.Text, nullable=False)
 #     category = db.Column(db.String(100), nullable=False)
 #
 #     def __repr__(self):
-#         return f"CartItem('{self.title}', 'S${self.price}', 'Date added:{self.date_added}')"
+#         return f"HackingProduct('{self.title}', 'S${self.price}', 'Date added:{self.date_added}')"
